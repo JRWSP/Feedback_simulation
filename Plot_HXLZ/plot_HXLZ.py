402ca1ff46b7830488313e80395043f9f1190560
Jirawat S <57433713+JRWSP@users.noreply.github.com>
2020-09-23 21:16:08 +0700

    Revised plots

--- Plot_HXLZ/plot_HXLZ.py
+++ Plot_HXLZ/plot_HXLZ.py
@@ -9,6 +9,7 @@ Created on Wed Aug 21 14:30:00 2019
 import numpy as np
 from qutip import *
 import matplotlib.pyplot as plt
+import string
 plt.rcParams["font.family"] = "serif"
 plt.rcParams["mathtext.fontset"] = "cm"
 plt.rcParams.update({'font.size': 24})
@@ -87,8 +88,8 @@ plt.setp(ax1.get_xticklabels(), visible=False)
 
 ax1.set_ylabel(r"$S_x$")
 lgd=ax1.legend(bbox_to_anchor=(1.01, 1), loc=2, borderaxespad=0.)
-ax1.set_ylim([-0.1, 1])
-
+ax1.set_ylim([-0.1, 1.0])
+ax1.text(-0.09, 0.9, "(a)", transform=ax1.transAxes, size=20)
 
 ax2.plot(x[:j], Lz[:j], '--', linewidth=l, label=r"$Lindblad$", color=palette(0))
 ax2.plot(x[:j], Mz[:j], '--', linewidth=l, label=r"$\tau = 0$", color=palette(1))
@@ -108,21 +109,24 @@ ax2.axvline(x[200], linewidth=1, color=palette(6))
 ax2.axvline(x[150], linewidth=1, color=palette(3))
 ax2.axvline(x[250], linewidth=1, color=palette(7))
 ax2.set_ylabel(r"$S_z$")
-ax2.set_ylim([-1, 1])
+ax2.set_ylim([-1.0, 1.0])
+ax2.text(-0.09, 0.9, "(b)", transform=ax2.transAxes, size=20)
 
-ax3.scatter(x[:j:k], Sim175[2][:j:k], marker='x', s=size, color=palette(4))
-ax3.plot(x[:j], Sim175[2][:j], linewidth=1, color=palette(4))
-ax3.axvline(x[175], linewidth=1, color=palette(4))
-ax3.plot(x[:j], Lz[:j], '--', linewidth=l, label=r"$Lindblad$", color=palette(0))
-ax3.plot(x[:j], Mz[:j], '--', linewidth=l, label=r"$\tau = 0$", color=palette(1))
-
+ax3.scatter(x[:j:k], Sim175[2][:j:k], marker='x', s=size, color=palette(4), label=r"$\tau = 1.75T_{\Omega}$")
+ax3.plot(x[:j], Sim175[2][:j], linewidth=1, color=palette(4), label='_nolegend_')
+ax3.axvline(x[175], linewidth=1, color=palette(4), label='_nolegend_')
+ax3.plot(x[:j], Lz[:j], '--', linewidth=l, label='_nolegend_', color=palette(0))
+ax3.plot(x[:j], Mz[:j], '--', linewidth=l, label='_nolegend_', color=palette(1))
+ax3.legend()
 plt.setp(ax1.get_xticklabels(), fontsize=6)
 
 ax3.set_ylabel(r"$S_z$")
-ax3.set_ylim([-1, 1])
+ax3.set_ylim([-1.0, 1.0])
+ax3.text(-0.09, 0.9, "(c)", transform=ax3.transAxes, size=20)
+
 plt.xlabel(r"Time (units of $T_{\Omega})$")
 
 plt.xticks([0, 0.1, 0.2, 0.3, 0.4, 0.5], ["0", r"$1.0$", r"$2.0$", r"$3.0$", r"$4.0$", r"$5.0$"])
-plt.tight_layout()
+plt.tight_layout(pad=1.0)
 #plt.show()
 plt.savefig("HXLZ_n", dpi=150, bbox_extra_artists=(lgd,), bbox_inches='tight')
